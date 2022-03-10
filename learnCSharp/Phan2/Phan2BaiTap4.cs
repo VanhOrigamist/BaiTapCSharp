006f0c362ed85826fa320b493e5b78cf9dd040b3
Nguyễn Việt Anh <96413432+VanhOrigamist@users.noreply.github.com>
2022-03-10 19:00:50 +0700

    Update Phan2BaiTap4.cs

--- learnCSharp/Phan2/Phan2BaiTap4.cs
+++ learnCSharp/Phan2/Phan2BaiTap4.cs
@@ -1,8 +1,24 @@
 using System;
-class Phan2BaiTap4 {
-    public static void bt4() {  
-        Console.WriteLine("\"Chào bạn!\"");
-        Console.WriteLine("\"Tôi tên là: Nguyen Van A\"");
-        Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
+namespace P2E4
+{
+    class CSharpNguVcl
+    {
+        static void Main(string[] args)
+        {   
+            double x1,x2,x3,y1,y2,y3;
+            Console.WriteLine("x1: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x2: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y2: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+            x3 = (x1+x2)/2;
+            y3 = (y1+y2)/2;
+            Console.WriteLine("("+x3+", "+y3+")");
+            Console.WriteLine(Math.Sqrt(Math.Pow((x1-x2),2)+Math.Pow((y1-y2),2)));
+            Console.ReadLine();
+        }
     }
-}
+}   
